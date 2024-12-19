e490aafe710fa746a9561cc94411e8c5aebc2ca7
izalu99 <117947026+izalu99@users.noreply.github.com>
2024-12-19 15:24:40 -0700

    updated server name in contants to match the env var

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,5 +1,5 @@
 
 export const BASE_URL = import.meta.env.VITE_BASE_URL;
-export const SERVER_URL = import.meta.env.VITE_REACT_APP_SERVER_URL
+export const SERVER_URL = import.meta.env.VITE_SERVER_URL
 
 export const PAYPAL_CLIENT_ID = import.meta.env.VITE_PAYPAL_CLIENT_ID
